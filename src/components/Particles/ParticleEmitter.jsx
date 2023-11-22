7ed51b087673483d2acb6ac4b5e814d158af879c
Roger Winright <rogerwinright@gmail.com>
2023-11-22 10:56:57 -0500

    add particles

---
+++ src/components/Particles/ParticleEmitter.jsx
@@ -0,0 +1,95 @@
+import React, { useCallback } from 'react'
+import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim";
+
+const ParticleEmitter = () => {
+  const particlesInit = useCallback(async engine => {
+    await loadSlim(engine);
+  }, [])
+
+  const particlesLoaded = useCallback(async container => {
+    console.log(container);
+  }, []);
+
+  return (
+    <div className="ParticleEmitter">
+
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "#f5f5f5",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 50,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#000000",
+            },
+            links: {
+              color: "#000000",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    </div>
+  )
+}
+
+export default ParticleEmitter
